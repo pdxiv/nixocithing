dd8859b76850a800925900277b95a3526cec0e92
pdxiv <pdxiv@github.com>
2022-11-27 12:51:32 +0100

    initial commit

---
+++ buildmyhello.nix
@@ -0,0 +1,16 @@
+with import <nixpkgs> {}; stdenv.mkDerivation {
+  name = "myhello";
+  src = ./src;
+  buildInputs = [ coreutils gcc ];
+
+  configurePhase = ''
+    declare -xp
+  '';
+  buildPhase = ''
+    gcc "$src/hello.c" -o ./hello
+  '';
+  installPhase = ''
+    mkdir -p "$out/bin"
+    cp ./hello "$out/bin/"
+  '';
+}
---
+++ src/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("This is my hello\n");
+    return 0;
+}
